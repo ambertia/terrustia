bc27f92565d94458ad32cf93ebdc9be6ebda5d2f
ambertia <95402631+ambertia@users.noreply.github.com>
2025-05-24 12:01:18 -0700

    Basic Bevy application with moving dot
    A Bevy app that has a plain black background, and a white dot that moves
    in response to WASD input.

---
+++ src/main.rs
@@ -0,0 +1,65 @@
+use bevy::prelude::*;
+
+// Dot's velocity in pixels per second
+const DOT_VELOCITY: f32 = 60.0;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .insert_resource(ClearColor(Color::BLACK))
+        .add_systems(Startup, setup)
+        .add_systems(Update, handle_dot)
+        .run();
+}
+
+#[derive(Component)]
+struct Dot;
+
+// Move the dot around the screen based on keyboard input
+fn handle_dot(
+    keyboard: Res<ButtonInput<KeyCode>>,
+    mut dot_transform: Single<&mut Transform, With<Dot>>,
+    time: Res<Time>,
+) {
+    let mut direction = Vec2::splat(0.0);
+
+    // Check all the keys and modify the dot's direction accordingly
+    if keyboard.pressed(KeyCode::KeyW) {
+        direction.y += 1.0;
+    }
+
+    if keyboard.pressed(KeyCode::KeyA) {
+        direction.x -= 1.0;
+    }
+
+    if keyboard.pressed(KeyCode::KeyS) {
+        direction.y -= 1.0;
+    }
+
+    if keyboard.pressed(KeyCode::KeyD) {
+        direction.x += 1.0;
+    }
+
+    println!("{}", direction);
+
+    let distance_potential = time.delta_secs() * DOT_VELOCITY;
+    dot_transform.translation.x += direction.x * distance_potential;
+    dot_transform.translation.y += direction.y * distance_potential;
+}
+
+// Initialize all the stuff in the world
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    commands.spawn(Camera2d);
+
+    // Spawn the dot
+    commands.spawn((
+        Dot,
+        Mesh2d(meshes.add(Circle::default())),
+        MeshMaterial2d(materials.add(Color::WHITE)),
+        Transform::default().with_scale(Vec2::splat(10.0).extend(1.)),
+    ));
+}
