81e651c35d8885d89cfcb01ad08df5ef93320d04
ambertia <95402631+ambertia@users.noreply.github.com>
2025-05-24 19:17:26 -0700

    Clean up code and constants, remove per-axis computations in favor of Vec2 functions

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,8 @@
 use bevy::prelude::*;
 
-// Dot's velocity in pixels per second
-const DOT_VELOCITY: f32 = 60.0;
-const DOT_ACCEL: f32 = 60.0;
-const DRAG_FACTOR: f32 = 0.4;
+const DOT_ACCEL: f32 = 30.0;
+const DRAG_FACTOR: f32 = 0.6;
 const VEL_MAX: f32 = 100.0;
-const VEL_MIN: f32 = 10.0;
 
 fn main() {
     App::new()
@@ -50,26 +47,12 @@ fn handle_dot(
 
     input_direction = input_direction.normalize_or_zero();
 
-    // Apply a decay to the velocity
-    if dot_vel.0.x.abs() < VEL_MIN {
-        dot_vel.0.x = 0.0;
-    } else {
-        dot_vel.0.x *= 1.0 - (DRAG_FACTOR * time.delta_secs());
-    }
-
-    if dot_vel.0.y.abs() < VEL_MIN {
-        dot_vel.0.y = 0.0;
-    } else {
-        dot_vel.0.y *= 1.0 - (DRAG_FACTOR * time.delta_secs());
-    }
-
-    // Apply acceleration to the dot's velocity
-    dot_vel.0.x += input_direction.x * DOT_ACCEL * time.delta_secs();
-    dot_vel.0.y += input_direction.y * DOT_ACCEL * time.delta_secs();
+    dot_vel.0 = (dot_vel.0.lerp(Vec2::ZERO, DRAG_FACTOR * time.delta_secs())
+        + (input_direction * DOT_ACCEL * time.delta_secs()))
+    .clamp_length_max(VEL_MAX);
 
     // Apply velocity to the dot's transform
-    dot_transform.translation.x += dot_vel.0.x * time.delta_secs();
-    dot_transform.translation.y += dot_vel.0.y * time.delta_secs();
+    dot_transform.translation += dot_vel.0.extend(0.0);
 }
 
 // Initialize all the stuff in the world
