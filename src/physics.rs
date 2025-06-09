0e05b7b9be90af286eaa6ff72feb62264dd19c2d
ambertia <95402631+ambertia@users.noreply.github.com>
2025-06-09 15:23:36 -0700

    Fill body of physics::accel_env()

--- src/physics.rs
+++ src/physics.rs
@@ -25,8 +25,18 @@ struct MovementState {
     position: Vec2,
     velocity: Vec2,
 }
-// TODO: Accelerate entities based on drag and gravity
-fn accel_env(movers: Query<&mut MovementState>, time_fixed: Res<Time<Fixed>>) {}
+
+const DRAG_FACTOR: f32 = 0.05;
+const GRAVITY: f32 = -15.;
+/// Accelerate entities based on drag and gravity
+fn accel_env(movers: Query<&mut MovementState>, time_fixed: Res<Time<Fixed>>) {
+    for mut mover in movers {
+        let drag_impulse = mover.velocity * DRAG_FACTOR * time_fixed.delta_secs();
+        let grav_impulse = GRAVITY * time_fixed.delta_secs();
+        mover.velocity += drag_impulse + vec2(0., grav_impulse);
+    }
+}
+
 // TODO: Apply input-based acceleration to the character
 fn accel_input(player: Single<&mut MovementState, With<Player>>, time_fixed: Res<Time<Fixed>>) {}
 
