671616f51404940aba60808bcf1013b804d218cc
ambertia <95402631+ambertia@users.noreply.github.com>
2025-06-10 15:41:43 -0700

    Finish block_collisions(), depending fn tiles_occupied()

--- src/physics.rs
+++ src/physics.rs
@@ -1,6 +1,7 @@
 use std::ops::Range;
 
 use bevy::{math::bounding::Aabb2d, prelude::*};
+use round_to::{CeilTo, FloorTo};
 
 use crate::terrain::GameMap;
 use crate::{Player, terrain};
@@ -27,11 +28,6 @@ impl Plugin for PhysicsPlugin {
 struct MovementState {
     position: Vec2,
     velocity: Vec2,
-}
-
-// TODO: Use round_to functions to return the indices of all possible colliding tiles
-impl MovementState {
-    fn tiles_occupied(&self) -> (Range<usize>, Range<usize>) {}
 }
 
 const DRAG_FACTOR: f32 = 0.05;
@@ -100,25 +96,32 @@ enum CollisionSide {
     Right,
 }
 
-// TODO: Check for collisions between entities and nearby solid objects, and fire a CollisionEvent
+/// Check for collisions between entities and nearby solid objects, and fire a CollisionEvent
 fn block_collisions(
-    movers: Query<&mut MovementState>,
+    movers: Query<(&MovementState, &Transform)>,
     game_map: Res<GameMap>,
     mut events: EventWriter<Collision>,
 ) {
-    for mut mover in movers {
+    for mover in movers {
         // Get the range of tile coordinates to check for blocks based on the mover's position and size
-        let (range_x, range_y) = mover.into_inner().tiles_occupied();
-        for x in range_x {
-            for y in range_y {
+        let (movement_state, transform) = mover;
+        let (range_x, range_y) = tiles_occupied(movement_state, transform);
+
+        // Iterate over the nearby blocks
+        // Have to Clone the ranges because they can't be Copy'd for implicit move (compiler whines)
+        for x in range_x.clone() {
+            for y in range_y.clone() {
+                // Fetch tile data. Disregard this tile if it isn't collidable
                 let tile = game_map.tile_at(x, y);
                 if !tile.has_solid() {
                     continue;
                 }
-                // TODO: This relies on the player's size for now
-                let height_diff = (crate::PLAYER_HEIGHT - crate::PLAYER_WIDTH) / 2.0;
-                let offset = mover.position
-                    - terrain::map_space_to_aabb2d(x, y).closest_point(mover.position);
+
+                // Fire a Collision event with the appropriate Side based on the closest collision
+                // point relative to entity center
+                let height_diff = (transform.scale.y - transform.scale.x) / 2.0;
+                let offset = movement_state.position
+                    - terrain::map_space_to_aabb2d(x, y).closest_point(movement_state.position);
                 if offset.x.abs() > offset.y.abs() - height_diff {
                     if offset.x < 0.0 {
                         events.write(Collision(CollisionSide::Right));
@@ -133,6 +136,18 @@ fn block_collisions(
             }
         }
     }
+}
+
+/// Return map coordinate ranges for all tiles at least partially occupied by the moving object.
+fn tiles_occupied(mover: &MovementState, transform: &Transform) -> (Range<usize>, Range<usize>) {
+    // Get corners of mover by adding scale to position
+    let top_right: Vec2 = mover.position + transform.scale.truncate() / 2.;
+    let bottom_left: Vec2 = mover.position - transform.scale.truncate() / 2.;
+    // Use round_to functions to return Range<usize>'s
+    return (
+        bottom_left.x.floor_to()..top_right.x.floor_to(),
+        bottom_left.y.ceil_to()..top_right.y.ceil_to(),
+    );
 }
 
 /// Move entities in world space
