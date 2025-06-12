7312492931d0379206383d21210eeac6b0522294
ambertia <95402631+ambertia@users.noreply.github.com>
2025-06-12 14:11:50 -0700

    impl Plugin for TerrainPlugin

--- src/terrain.rs
+++ src/terrain.rs
@@ -6,6 +6,15 @@ use bevy::{
 
 use crate::BLOCK_SIZE;
 
+pub struct TerrainPlugin;
+
+impl Plugin for TerrainPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(FixedUpdate, tile_modifications)
+            .add_systems(Update, tile_sprite_updates);
+    }
+}
+
 #[derive(Resource)]
 pub struct GameMap(HashMap<(i16, i16), TileData>);
 
