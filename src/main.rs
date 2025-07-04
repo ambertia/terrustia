766711a3ecb8d67639c3942c72dd4f283fbb0fed
ambertia <95402631+ambertia@users.noreply.github.com>
2025-07-04 11:59:06 -0700

    Move player spawning to player module

--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,6 @@ mod player;
 mod terrain;
 mod ui;
 
-const PLAYER_HEIGHT: f32 = 3.0;
-const PLAYER_WIDTH: f32 = 2.0;
-
 fn main() {
     App::new()
         .add_plugins((
@@ -25,34 +22,5 @@ fn main() {
         ))
         .insert_resource(ClearColor(Color::BLACK))
         .insert_resource(Gravity(Vec2::NEG_Y * 50.))
-        .add_systems(Startup, setup)
         .run();
-}
-
-// Initialize all the stuff in the world
-fn setup(mut commands: Commands) {
-    // Spawn the player
-    commands.spawn((
-        Player,
-        RigidBody::Dynamic,
-        Collider::rectangle(PLAYER_WIDTH - 0.1, PLAYER_HEIGHT - 0.1),
-        Sprite {
-            color: Color::from(WHITE),
-            custom_size: Some(Vec2::new(PLAYER_WIDTH, PLAYER_HEIGHT)),
-            ..default()
-        },
-        Transform::from_xyz(0., 30., 1.),
-        // A ShapeCaster to help detect if the player is touching the ground.
-        ShapeCaster::new(
-            Collider::rectangle(PLAYER_WIDTH * 0.99, PLAYER_HEIGHT * 0.99),
-            Vector::ZERO,
-            0.,
-            Dir2::NEG_Y,
-        )
-        .with_max_distance(0.1),
-        LockedAxes::ROTATION_LOCKED,
-        Friction::new(0.1).with_combine_rule(CoefficientCombine::Min),
-        CollisionMargin(0.05),
-        LinearDamping(0.1),
-    ));
 }
--- src/player.rs
+++ src/player.rs
@@ -1,5 +1,8 @@
 use avian2d::{math::Vector, prelude::*};
-use bevy::{color::palettes::tailwind::GRAY_950, prelude::*};
+use bevy::{
+    color::palettes::{css::WHITE, tailwind::GRAY_950},
+    prelude::*,
+};
 
 pub struct CharacterControllerPlugin;
 
@@ -72,6 +75,35 @@ fn keyboard_input(
             player_vel.y = JUMP_VEL;
         }
     }
+}
+
+const PLAYER_WIDTH: f32 = 2.;
+const PLAYER_HEIGHT: f32 = 3.;
+
+fn spawn_player(mut commands: Commands) {
+    commands.spawn((
+        Player,
+        RigidBody::Dynamic,
+        Collider::rectangle(PLAYER_WIDTH - 0.1, PLAYER_HEIGHT - 0.1),
+        Sprite {
+            color: Color::from(WHITE),
+            custom_size: Some(Vec2::new(PLAYER_WIDTH, PLAYER_HEIGHT)),
+            ..default()
+        },
+        Transform::from_xyz(0., 30., 1.),
+        // A ShapeCaster to help detect if the player is touching the ground.
+        ShapeCaster::new(
+            Collider::rectangle(PLAYER_WIDTH * 0.99, PLAYER_HEIGHT * 0.99),
+            Vector::ZERO,
+            0.,
+            Dir2::NEG_Y,
+        )
+        .with_max_distance(0.1),
+        LockedAxes::ROTATION_LOCKED,
+        Friction::new(0.1).with_combine_rule(CoefficientCombine::Min),
+        CollisionMargin(0.05),
+        LinearDamping(0.1),
+    ));
 }
 
 #[derive(Resource, Default)]
